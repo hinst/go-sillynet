d8e8b80a68e940f6faff12c804ccf950b2df776e
hinst <hinstance@yandex.ru>
2015-03-15 12:46:51 +0300

    sillynet in progress

--- src/h_sillynet/sillynet.go
+++ src/h_sillynet/sillynet.go
@@ -10,20 +10,21 @@ type Thread struct {
 	Active    bool
 }
 
-func StartThread(f func(thread *Thread)) Thread {
-	var thread Thread
+func StartThread(f func(thread *Thread)) *Thread {
+	var thread = &Thread{}
 	thread.waitGroup.Add(1)
 	go func() {
 		f(thread)
 		thread.waitGroup.Done()
 	}()
+	return thread
 }
 
 // "Simple" means that it has a single access point. Only one client can connect.
 type SimpleServer struct {
 	Port            int
 	listener        net.TCPListener
-	acceptionThread sync.WaitGroup
+	acceptionThread *Thread
 	Active          bool
 }
 
@@ -32,19 +33,20 @@ func (simpleServer *SimpleServer) ClientAcceptionRoutine(thread *Thread) {
 		simpleServer.listener.SetDeadline(time.Now().Add(1 * time.Second))
 		var acceptedConnection, acceptResult = simpleServer.listener.Accept()
 		if nil == acceptResult /*success*/ {
+
 			break
 		}
 	}
 }
 
 func (simpleServer *SimpleServer) Start() bool {
-	if false == Active {
+	if false == simpleServer.Active {
 	}
 	var result = false
-	var listener, listenResult = net.Listen("tcp", ":"+strconv.Itoa(port))
+	var listener, listenResult = net.Listen("tcp", ":"+strconv.Itoa(simpleServer.Port))
 	if nil == listenResult /*success*/ {
 		var simpleServer = &SimpleServer{}
-		simpleServer.listener = listener
+		simpleServer.listener = listener.(net.TCPListener)
 		simpleServer.Active = true
 		simpleServer.acceptionThread = StartThread(simpleServer.ClientAcceptionRoutine)
 		result = true
