6d3e9711f7986539b18aa1419e5641094e29a7a7
hinst <hinstance@yandex.ru>
2015-03-21 19:32:58 +0300

    Client reader thread should be functional

--- src/h_sillynet/client.go
+++ src/h_sillynet/client.go
@@ -4,13 +4,12 @@ import "net"
 import "time"
 
 type Client struct {
-	messageReceiver  MessageReceiver
-	connectionActive bool
-	connection       net.Conn
-	readerThread     *Thread
-	writerThread     *Thread
-	incoming         MemoryBlockQueue
-	outgoing         MemoryBlockQueue
+	messageReceiver MessageReceiver
+	connection      net.Conn
+	readerThread    *Thread
+	writerThread    *Thread
+	incoming        MemoryBlockQueue
+	outgoing        MemoryBlockQueue
 }
 
 func (this *Client) Push(memoryBlock []byte) {
@@ -21,19 +20,39 @@ func (this *Client) Pop() []byte {
 	return this.incoming.Pop()
 }
 
+func CheckIfNetTimeoutError(e error) bool {
+	var netError, castResult = readResult.(net.Error)
+	if castResult {
+		result = netError.Timeout()
+	} else {
+		result = false
+	}
+}
+
 func (this *Client) start() {
-	var readerThreadRoutine = func(thread *Thread) {
-		var readForward = func() {
-			this.connection.SetDeadline(time.Now().Add(1 * time.Second))
-			var buffer = make([]byte, 1, 1)
-			var readLength, readResult = this.connection.Read(buffer)
-			if readResult == nil {
-				if readLength == 1 {
-				}
-			}
+	var buffer = make([]byte, 1, 1)
+	var tryExtractMessage = func() {
+		var incomingMessage = this.messageReceiver.Extract()
+		if incomingMessage != nil {
+			this.incoming.Push(incomingMessage)
 		}
+	}
+	var readForward = func() {
+		this.connection.SetDeadline(time.Now().Add(1 * time.Second))
+		var readLength, readResult = this.connection.Read(buffer)
+		if readResult == nil {
+			if readLength == 1 {
+				this.messageReceiver.Write(buffer[0])
+				tryExtractMessage()
+			}
+		} else if false == CheckIfNetTimeoutError(readResult) {
+			this.connection.Close()
+			this.connection = nil
+		}
+	}
+	var readerThreadRoutine = func(thread *Thread) {
 		for thread.Active {
-			if this.connectionActive {
+			if this.ConnectionActive() {
 				readForward()
 			}
 		}
@@ -44,8 +63,17 @@ func (this *Client) start() {
 	this.writerThread = StartThread(writerThreadRoutine)
 }
 
-func (this *Client) Accept(connection net.Conn) {
-	this.connection = connection
-	this.connectionActive = true
-	this.start()
+func (this *Client) Accept(connection net.Conn) bool {
+	if this.connection == nil {
+		this.connection = connection
+		this.connectionActive = true
+		this.start()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (this *Client) ConnectionActive() bool {
+	return this.connection != nil
 }
